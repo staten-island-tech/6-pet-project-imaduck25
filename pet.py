907e575f316bea6f19bd38c9f5fd8e5fbf9e3442
imaduck25 <fatimaa158@nycstudents.net>
2025-12-02 06:44:29 -0800

    Update pet.py

---
+++ pet.py
@@ -0,0 +1,18 @@
+
+def yournewpet():
+    def __init__(self, name, energy, happiness):
+        self.name = name
+        self.energy = energy
+        self.happiness = happiness
+    namee = []
+    pet_name = input("Hi! You have just been gifted a new pet! The species is unkown, but pick a name for it! ")
+    namee.append(pet_name)
+    
+yournewpet()
+
+
+
+
+
+""" 
+results.append(item["title"]) """
